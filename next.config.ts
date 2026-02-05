60f850459d2c9b9f324e7686eab593050658357e
sahab <sahabranjbar.dev@gmail.com>
2026-02-05 05:16:20 +0330

    add server action to nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000", "*.vercel.app"],
+    },
+  },
 };
 
 export default nextConfig;
