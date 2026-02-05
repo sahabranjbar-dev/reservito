209d06589b7db5ca7cf690ff178f611cd3982138
sahab <sahabranjbar.dev@gmail.com>
2026-02-05 04:09:03 +0330

    Add homepage for Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  reactStrictMode: true,
+  experimental: {
+    // @ts-expect-error appDir هنوز در types تعریف نشده
+    appDir: true, // 🔹 این مهمه
+  },
 };
 
 export default nextConfig;
